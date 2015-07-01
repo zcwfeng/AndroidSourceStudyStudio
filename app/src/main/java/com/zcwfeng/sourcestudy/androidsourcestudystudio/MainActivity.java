22d5abf2d6c618e20cd2e3a1c19e1a20f499355c
bingdongguke <zcwfeng@gmail.com>
2015-07-02 01:28:10 +0800

    android annotations

--- app/src/main/java/com/zcwfeng/sourcestudy/androidsourcestudystudio/MainActivity.java
+++ app/src/main/java/com/zcwfeng/sourcestudy/androidsourcestudystudio/MainActivity.java
@@ -1,50 +1,64 @@
 package com.zcwfeng.sourcestudy.androidsourcestudystudio;
 
 import android.content.Intent;
+import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.concurrent.TimeUnit;
+//http://plugins.jetbrains.com/plugin/7369
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.test_stick_nav_layout)
+    Button mBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.test_stick_nav_layout).setOnClickListener(this);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+
+    @Background
+        // Executed in a background thread
+    void translateInBackground(String textToTranslate) {
+        String translatedText = callGoogleTranslate(textToTranslate);
+        showResult(translatedText);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public String callGoogleTranslate(String txt){
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
+        return "demo";
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.test_stick_nav_layout:
-                Intent intent = new Intent(this,com.zcwfeng.sourcestudy.androidsourcestudystudio.stickynavlayout.StickNavLayoutMainActivity.class);
-                startActivity(intent);
-                break;
-        }
+    @UiThread
+    void showResult(String translatedText) {
+        mBtn.setText(translatedText);
     }
+
+
+    @Click(R.id.test_stick_nav_layout)
+    public void mBtnClick() {
+        Intent intent = new Intent(this, com.zcwfeng.sourcestudy.androidsourcestudystudio.stickynavlayout.StickNavLayoutMainActivity.class);
+        startActivity(intent);
+    }
+
 }
